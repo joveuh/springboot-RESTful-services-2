db2b16b1597a1851731a0894965242fa388b6bca
Uzair <uhass924@gmail.com>
2022-10-31 19:57:45 -0400

    Enhanced POST method to return correct HTTP Status Code i.e. 201 for Created() and returned a Location for the created resource

--- src/main/java/com/learningspringboot/RESTfulApp/users/UserResource.java
+++ src/main/java/com/learningspringboot/RESTfulApp/users/UserResource.java
@@ -1,34 +1,41 @@
 package com.learningspringboot.RESTfulApp.users;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.factory.parsing.Location;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
-    
+
     private UsersDaoService service;
 
-    public UserResource(UsersDaoService service){
+    public UserResource(UsersDaoService service) {
         this.service = service;
     }
 
     @GetMapping("users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
     }
 
     @PostMapping("users")
-    public void createUser(@RequestBody User user){
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
