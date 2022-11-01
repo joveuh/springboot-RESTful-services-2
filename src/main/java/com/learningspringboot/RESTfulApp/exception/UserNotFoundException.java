094386bea90f8fecd68bcbe2bb553d08206d012c
Uzair H <uzair@ibm.com>
2022-11-01 10:00:49 -0400

    Moved exception to new package

--- src/main/java/com/learningspringboot/RESTfulApp/exception/UserNotFoundException.java
+++ src/main/java/com/learningspringboot/RESTfulApp/exception/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package com.learningspringboot.RESTfulApp.users;
+package com.learningspringboot.RESTfulApp.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- src/main/java/com/learningspringboot/RESTfulApp/users/UserResource.java
+++ src/main/java/com/learningspringboot/RESTfulApp/users/UserResource.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.learningspringboot.RESTfulApp.exception.UserNotFoundException;
+
 @RestController
 public class UserResource {
 
