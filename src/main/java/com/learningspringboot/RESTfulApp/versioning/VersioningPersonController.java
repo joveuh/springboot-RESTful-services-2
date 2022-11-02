13b50316b02c9ff2c15fb334a744942d79e01086
Uzair <uhass924@gmail.com>
2022-11-01 20:58:53 -0400

    We can use different methods to use versioning. We can implement versioning based on URIs, request parameters, or headers

--- src/main/java/com/learningspringboot/RESTfulApp/versioning/VersioningPersonController.java
+++ src/main/java/com/learningspringboot/RESTfulApp/versioning/VersioningPersonController.java
@@ -7,13 +7,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class VersioningPersonController {
 
     @GetMapping("/v1/person")
-    public PersonV1 getFirstVersionOfPerson(){
+    public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Bob Charlie");
     }
-    
+
     @GetMapping("/v2/person")
-    public PersonV2 getSecondVersionOfPerson(){
+    public PersonV2 getSecondVersionOfPerson() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getVersionedPerson1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getVersionedPerson2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person/header", headers = "RANDOM-HEADER=1")
+    public PersonV1 getHeaderedPerson1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "RANDOM-HEADER=2")
+    public PersonV2 getHeaderedPerson2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person/accpet", produces = "application/vndtest")
+    public PersonV2 getVerionedPersonAcceptHeader() {
+        return new PersonV2(new Name("Test", "Charlie"));
+    }
 }
